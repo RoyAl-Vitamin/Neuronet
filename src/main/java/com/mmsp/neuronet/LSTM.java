289a03530116ef941ef13e9c24c499e594a8ddb9
RoyAl-Vitamin <roalvi95@gmail.com>
2017-02-09 23:01:37 +0500

    Добавлено сохранение и загрузка нейросети

--- src/main/java/com/mmsp/neuronet/LSTM.java
+++ src/main/java/com/mmsp/neuronet/LSTM.java
@@ -1,5 +1,11 @@
 package com.mmsp.neuronet;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.Random;
+
 import org.apache.commons.io.FileUtils;
 import org.deeplearning4j.nn.api.Layer;
 import org.deeplearning4j.nn.api.OptimizationAlgorithm;
@@ -12,19 +18,18 @@ import org.deeplearning4j.nn.conf.layers.RnnOutputLayer;
 import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
 import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.deeplearning4j.util.ModelSerializer;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.factory.Nd4j;
 import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.util.Random;
-
 public class LSTM {
+	
+	private final static boolean NEED_LOAD_NET = false;
+	
+	private final static String PATH_TO_NET = "MyMultiLayerNetwork.zip";
 
 	/**GravesLSTM Character modelling example
 	 * @author Alex Black
@@ -40,89 +45,96 @@ public class LSTM {
 	    http://deeplearning4j.org/recurrentnetwork
 	 */
 	public static void main(String[] args) {
-		int lstmLayerSize = 200;					//Number of units in each GravesLSTM layer
-		int miniBatchSize = 32;						//Size of mini batch to use when  training
-		int exampleLength = 1000;					//Length of each training example sequence to use. This could certainly be increased
-        int tbpttLength = 50;                       //Length for truncated backpropagation through time. i.e., do parameter updates ever 50 characters
-		int numEpochs = 1;							//Total number of training epochs
-        int generateSamplesEveryNMinibatches = 10;  //How frequently to generate samples from the network? 1000 characters / 50 tbptt length: 20 parameter updates per minibatch
-		int nSamplesToGenerate = 4;					//Number of samples to generate after each training epoch
-		int nCharactersToSample = 300;				//Length of each sample to generate
-		String generationInitialization = null;		//Optional character initialization; a random character is used if null
-		// Above is Used to 'prime' the LSTM with a character sequence to continue/complete.
-		// Initialization characters must all be in CharacterIterator.getMinimalCharacterSet() by default
-		Random rng = new Random(12345);
 
-		//Get a DataSetIterator that handles vectorization of text into something we can use to train
-		// our GravesLSTM network.
-		CharacterIterator iter = null;
-		try {
-			iter = getShakespeareIterator(miniBatchSize,exampleLength);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		MultiLayerNetwork net;
+
+		if (NEED_LOAD_NET) { // Load NET
+			net = loadNet(new File(PATH_TO_NET));
+		} else { // Create NET
+			int lstmLayerSize = 200;					//Number of units in each GravesLSTM layer
+			int miniBatchSize = 32;						//Size of mini batch to use when  training
+			int exampleLength = 1000;					//Length of each training example sequence to use. This could certainly be increased
+	        int tbpttLength = 50;                       //Length for truncated backpropagation through time. i.e., do parameter updates ever 50 characters
+			int numEpochs = 1;							//Total number of training epochs
+	        int generateSamplesEveryNMinibatches = 10;  //How frequently to generate samples from the network? 1000 characters / 50 tbptt length: 20 parameter updates per minibatch
+			int nSamplesToGenerate = 4;					//Number of samples to generate after each training epoch
+			int nCharactersToSample = 300;				//Length of each sample to generate
+			String generationInitialization = null;		//Optional character initialization; a random character is used if null
+			// Above is Used to 'prime' the LSTM with a character sequence to continue/complete.
+			// Initialization characters must all be in CharacterIterator.getMinimalCharacterSet() by default
+			Random rng = new Random(12345);
+
+			//Get a DataSetIterator that handles vectorization of text into something we can use to train
+			// our GravesLSTM network.
+			CharacterIterator iter = null;
+			try {
+				iter = getShakespeareIterator(miniBatchSize, exampleLength);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			int nOut = iter.totalOutcomes();
+
+			//Set up network configuration:
+			MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+				.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT).iterations(1)
+				.learningRate(0.1)
+				.rmsDecay(0.95)
+				.seed(12345)
+				.regularization(true)
+				.l2(0.001)
+	            .weightInit(WeightInit.XAVIER)
+	            .updater(Updater.RMSPROP)
+				.list()
+				.layer(0, new GravesLSTM.Builder().nIn(iter.inputColumns()).nOut(lstmLayerSize)
+						.activation(Activation.TANH).build())
+				.layer(1, new GravesLSTM.Builder().nIn(lstmLayerSize).nOut(lstmLayerSize)
+						.activation(Activation.TANH).build())
+				.layer(2, new RnnOutputLayer.Builder(LossFunction.MCXENT).activation(Activation.SOFTMAX)        //MCXENT + softmax for classification
+						.nIn(lstmLayerSize).nOut(nOut).build())
+	            .backpropType(BackpropType.TruncatedBPTT).tBPTTForwardLength(tbpttLength).tBPTTBackwardLength(tbpttLength)
+				.pretrain(false).backprop(true)
+				.build();
+
+			net = new MultiLayerNetwork(conf);
+			net.init();
+			net.setListeners(new ScoreIterationListener(1));
+
+			//Print the  number of parameters in the network (and for each layer)
+			Layer[] layers = net.getLayers();
+			int totalNumParams = 0;
+			for( int i = 0; i < layers.length; i++ ){
+				int nParams = layers[i].numParams();
+				System.out.println("Number of parameters in layer " + i + ": " + nParams);
+				totalNumParams += nParams;
+			}
+			System.out.println("Total number of network parameters: " + totalNumParams);
+
+			//Do training, and then generate and print samples from network
+	        int miniBatchNumber = 0;
+			for( int i=0; i < numEpochs; i++ ){
+	            while(iter.hasNext()){
+	                DataSet ds = iter.next();
+	                net.fit(ds);
+	                if(++miniBatchNumber % generateSamplesEveryNMinibatches == 0){
+	                    System.out.println("--------------------");
+	                    System.out.println("Completed " + miniBatchNumber + " minibatches of size " + miniBatchSize + "x" + exampleLength + " characters" );
+	                    System.out.println("Sampling characters from network given initialization \"" + (generationInitialization == null ? "" : generationInitialization) + "\"");
+	                    String[] samples = sampleCharactersFromNetwork(generationInitialization,net,iter,rng,nCharactersToSample,nSamplesToGenerate);
+	                    for( int j=0; j<samples.length; j++ ){
+	                        System.out.println("----- Sample " + j + " -----");
+	                        System.out.println(samples[j]);
+	                        System.out.println();
+	                    }
+	                }
+	            }
+
+				iter.reset();	//Reset iterator for another epoch
+			}
+
+			System.out.println("\n\nExample complete");
+			saveNet(net, new File(PATH_TO_NET));
 		}
-		int nOut = iter.totalOutcomes();
-
-		//Set up network configuration:
-		MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-			.optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT).iterations(1)
-			.learningRate(0.1)
-			.rmsDecay(0.95)
-			.seed(12345)
-			.regularization(true)
-			.l2(0.001)
-            .weightInit(WeightInit.XAVIER)
-            .updater(Updater.RMSPROP)
-			.list()
-			.layer(0, new GravesLSTM.Builder().nIn(iter.inputColumns()).nOut(lstmLayerSize)
-					.activation(Activation.TANH).build())
-			
-			.layer(1, new GravesLSTM.Builder().nIn(lstmLayerSize).nOut(lstmLayerSize)
-					.activation(Activation.TANH).build())
-			.layer(2, new RnnOutputLayer.Builder(LossFunction.MCXENT).activation(Activation.SOFTMAX)        //MCXENT + softmax for classification
-					.nIn(lstmLayerSize).nOut(nOut).build())
-            .backpropType(BackpropType.TruncatedBPTT).tBPTTForwardLength(tbpttLength).tBPTTBackwardLength(tbpttLength)
-			.pretrain(false).backprop(true)
-			.build();
-
-		MultiLayerNetwork net = new MultiLayerNetwork(conf);
-		net.init();
-		net.setListeners(new ScoreIterationListener(1));
-
-		//Print the  number of parameters in the network (and for each layer)
-		Layer[] layers = net.getLayers();
-		int totalNumParams = 0;
-		for( int i=0; i<layers.length; i++ ){
-			int nParams = layers[i].numParams();
-			System.out.println("Number of parameters in layer " + i + ": " + nParams);
-			totalNumParams += nParams;
-		}
-		System.out.println("Total number of network parameters: " + totalNumParams);
-
-		//Do training, and then generate and print samples from network
-        int miniBatchNumber = 0;
-		for( int i=0; i<numEpochs; i++ ){
-            while(iter.hasNext()){
-                DataSet ds = iter.next();
-                net.fit(ds);
-                if(++miniBatchNumber % generateSamplesEveryNMinibatches == 0){
-                    System.out.println("--------------------");
-                    System.out.println("Completed " + miniBatchNumber + " minibatches of size " + miniBatchSize + "x" + exampleLength + " characters" );
-                    System.out.println("Sampling characters from network given initialization \"" + (generationInitialization == null ? "" : generationInitialization) + "\"");
-                    String[] samples = sampleCharactersFromNetwork(generationInitialization,net,iter,rng,nCharactersToSample,nSamplesToGenerate);
-                    for( int j=0; j<samples.length; j++ ){
-                        System.out.println("----- Sample " + j + " -----");
-                        System.out.println(samples[j]);
-                        System.out.println();
-                    }
-                }
-            }
-
-			iter.reset();	//Reset iterator for another epoch
-		}
-
-		System.out.println("\n\nExample complete");
 	}
 	
 	/** Downloads Shakespeare training data and stores it locally (temp directory). Then set up and return a simple
@@ -130,7 +142,7 @@ public class LSTM {
 	 * @param miniBatchSize Number of text segments in each training mini-batch
 	 * @param sequenceLength Number of characters in each text segment.
 	 */
-	public static CharacterIterator getShakespeareIterator(int miniBatchSize, int sequenceLength) throws Exception{
+	private static CharacterIterator getShakespeareIterator(int miniBatchSize, int sequenceLength) throws Exception{
 		//The Complete Works of William Shakespeare
 		//5.3MB file in UTF-8 Encoding, ~5.4 million characters
 		//https://www.gutenberg.org/ebooks/100
@@ -220,6 +232,30 @@ public class LSTM {
 		}
 		//Should never happen if distribution is a valid probability distribution
 		throw new IllegalArgumentException("Distribution is invalid? d="+d+", sum="+sum);
+	}
+	
+	private static void saveNet(MultiLayerNetwork net, File locationToSave) {
+		//Save the model
+		//File locationToSave = new File("MyMultiLayerNetwork.zip"); // Where to save the network. Note: the file is in .zip format - can be opened externally
+	    boolean saveUpdater = true; // Updater: i.e., the state for Momentum, RMSProp, Adagrad etc. Save this if you want to train your network more in the future
+	    try {
+			ModelSerializer.writeModel(net, locationToSave, saveUpdater);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/** Load the model
+	 * @param locationToSave File
+	 * @return MultiLayerNetwork
+	 */
+	private static MultiLayerNetwork loadNet(File locationToSave) {
+		try {
+			return ModelSerializer.restoreMultiLayerNetwork(locationToSave);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
 }
